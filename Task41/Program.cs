b4818b9d42dff54afb5daa3ab94c008f6f83ab2b
egor27032005 <sergeevegor2005@gmail.com>
2022-09-25 11:46:32 -0400

    сделал дз

---
+++ Task41/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число m: ");
+int m =Convert.ToInt32(Console.ReadLine());
+int sum=0;
+int ConvertNumber(int i)
+{
+while(m>i)
+{
+    Console.Write("Введите число: ");
+    int x =Convert.ToInt32(Console.ReadLine());
+    i++;
+    if (x>0)
+    sum++;
+}
+return sum;
+}
+Console.WriteLine( "Положительных чисел: " + ConvertNumber(sum));
---
+++ Task43/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите значение k1: ");
+int k1= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b1: ");
+int b1= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+int k2= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+int b2= Convert.ToInt32(Console.ReadLine());
+double x = (b1-b2)/(k2-k1);
+double y =k1*x+b1;
+Console.WriteLine("("+x+"," + y + ")");
